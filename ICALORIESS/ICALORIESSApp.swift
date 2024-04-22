6cf3a50cc7ed84897ebcd624468d5810e7f7d1ac
MurodjonT <murodjon.mmm.99@mail.ru>
2024-04-22 11:49:10 +0500

    Initial Commit

---
+++ ICALORIESS/ICALORIESSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICALORIESSApp.swift
+//  ICALORIESS
+//
+//  Created by Murodjon Turobov on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ICALORIESSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
